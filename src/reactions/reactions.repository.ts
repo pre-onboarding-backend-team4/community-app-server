1030d520cc21ba1e87ff529e50a278b0b67b9074
azure928 <azure6.dev@gmail.com>
2022-10-14 07:55:11 +0900

    Add: 포스트에 리액션 남기기 기능 구현

---
+++ src/reactions/reactions.repository.ts
@@ -0,0 +1,20 @@
+import { Repository } from 'typeorm';
+import { Reaction } from '../posts/posts.entity';
+import { CreateReactionDto } from './createReactionDto';
+import { CustomRepository } from '../typeorm-ex.decorator';
+
+@CustomRepository(Reaction)
+export class ReactionRepository extends Repository<Reaction> {
+  async createReaction(createReactionDto: CreateReactionDto) {
+    const { type, user_id, post_id } = createReactionDto;
+
+    const reaction = this.create({
+      type,
+      user_id,
+      post_id,
+    });
+
+    await this.save(reaction);
+    return reaction;
+  }
+}
